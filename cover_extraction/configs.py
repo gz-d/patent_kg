8aeefd10e617c3e374bf8c5dcecc114dc1854279
williamxieigs <yi.xie@igsl-group.com>
2025-03-10 18:11:58 +0800

    update

--- cover_extraction/configs.py
+++ cover_extraction/configs.py
@@ -6,3 +6,7 @@ huggingface_api_key = ''
 host = "0.0.0.0"
 port = 8081
 
+neo4j_host = "bolt://localhost:7687"
+neo4j_user = "neo4j"
+neo4j_password = "12345678"
+neo4j_db = "neo4j"
--- cover_extraction/main.py
+++ cover_extraction/main.py
@@ -5,8 +5,10 @@ from os import walk
 from os.path import splitext, exists, join
 import fitz # pymupdf
 import json
+import re
 import numpy as np
 from models import Qwen25VL7B_dashscope, Qwen25VL7B_tgi, Qwen25VL7B_transformers
+from neo4j import GraphDatabase
 from configs import *
 
 FLAGS = flags.FLAGS
@@ -17,6 +19,8 @@ def add_options():
   flags.DEFINE_enum('device', default = 'cuda', enum_values = {'cpu', 'cuda'}, help = 'device to use')
 
 def main(unused_argv):
+  driver = GraphDatabase.driver(neo4j_host, auth = (neo4j_user, neo4j_password))
+  pattern = r'```json(.*?)```|```(.*?)```'
   for root, subFolders, files in walk(FLAGS.input_dir):
     for file in files:
       stem, ext = splitext(file)
@@ -24,7 +28,7 @@ def main(unused_argv):
       # 1) read first page of patent
       pdf = fitz.open(join(root, file))
       page = pdf[0]
-      pix = page.get_pixmap(dpi=200)
+      pix = page.get_pixmap(dpi=100)
       img = np.frombuffer(pix.samples, dtype = np.uint8).reshape(pix.height, pix.width, -1)
       if pix.n == 1: img = img[:,:,0]
       elif pix.n == 3: img = img.reshape(pix.height, pix.width, 3)
@@ -49,7 +53,26 @@ def main(unused_argv):
   "cited_patents": [<cited patent num1>, <cited patent num2>, ...],
   "fields of classification search": [<field1>, <field2>]
 }""", img)
-      import pdb; pdb.set_trace()
+      matches = re.findall(pattern, response, re.DOTALL)
+      if len(matches) < 1: continue
+      try:
+        info = json.loads(matches[0][0])
+      except:
+        continue
+    driver.execute_query('merge (a: Patent {patent_num: $pno, patent_name: $pnm}) return a;', pno = info['patent_num'], pnm = info['patent_name'], database_ = neo4j_db)
+    if 'applicant' in info:
+      driver.execute_query('merge (a: Applicant {name: $name}) return a;', name = info['applicant'], database_ = neo4j_db)
+      driver.execute_query('match (a: Patent {patent_num: $pno}), (b: Applicant {name: $name}) merge (a)<-[:APPLY]-(b);', pno = info['patent_num'], pnm = info['patent_name'], database_ = neo4j_db)
+    if 'inventors' in info and len(info['inventors']):
+      for inventor in info['inventors']:
+        driver.execute_query('merge (a: Inventor {name: $name}) return a;', name = inventor, database_ = neo4j_db)
+        driver.execute_query('match (a: Patent {patent_num: $pno}), (b: Inventor {name: $name}) merge (a)<-[:INVENT]-(b);', pno = info['patent_num'], name = inventor, database_ = neo4j_db)
+    if 'assignee' in info:
+      driver.execute_query('merge (a: Assignee {name: $name}) return a;', name = info['assignee'], database_ = neo4j_db)
+    if 'fields' in info and len(info['fields']):
+      for field in info['fields']:
+        driver.execute_query('merge (a: Field {name: $name}) return a;', name = field, database_ = neo4j_db)
+        driver.execute_query('match (a: Patent {patent_num: $pno}), (b: Field {name: $name}) merge (a)-[:BELONGS_TO]-(b);', pno = info['patent_num'], name = field, database_ = neo4j_db)
 
 if __name__ == "__main__":
   add_options()
