2265614ea50f37b1a25ef0fc275b0905d9a0c9cf
williamxieigs <yi.xie@igsl-group.com>
2025-03-10 15:18:48 +0800

    update

--- cover_extraction/main.py
+++ cover_extraction/main.py
@@ -5,6 +5,7 @@ from os import walk
 from os.path import splitext, exists, join
 import fitz # pymupdf
 import json
+import numpy as np
 from models import Qwen25VL7B_dashscope, Qwen25VL7B_tgi, Qwen25VL7B_transformers
 from configs import *
 
@@ -24,7 +25,10 @@ def main(unused_argv):
       pdf = fitz.open(join(root, file))
       page = pdf[0]
       pix = page.get_pixmap(dpi=200)
-      img = np.frombuffer(pix.samples, dtype = np.int8).reshape(pix.height, pix.width, -1)
+      img = np.frombuffer(pix.samples, dtype = np.uint8).reshape(pix.height, pix.width, -1)
+      if pix.n == 1: img = img[:,:,0]
+      elif pix.n == 3: img = img.reshape(pix.height, pix.width, 3)
+      elif pix.n == 4: img = img.reshape(pix.height, pix.width, 4)
       # 2) call vqa model to extract
       if FLAGS.api == 'transformers':
         model = Qwen25VL7B_transformers(huggingface_api_key)
