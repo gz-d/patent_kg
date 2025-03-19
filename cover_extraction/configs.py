6738173710145817387b24e9b65c248860f138cb
gz-d <90494164+gz-d@users.noreply.github.com>
2025-03-19 21:46:45 +0800

    Update configs.py

--- cover_extraction/configs.py
+++ cover_extraction/configs.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 dashscope_api_key = ''
-tgi_host = "http://localhost:9091"
-huggingface_token = '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'
-# huggingface_token = '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'
+tgi_host = ""
+huggingface_token = ''
 host = "0.0.0.0"
 port = 8081
 
-neo4j_host = "bolt://localhost:7687"
-neo4j_user = "neo4j"
-neo4j_password = "12345678"
-neo4j_db = "neo4j"
+neo4j_host = ""
+neo4j_user = ""
+neo4j_password = ""
+neo4j_db = ""
