a8b45888923235ac62d286ebe0c28167915f640d
gz-d <90494164+gz-d@users.noreply.github.com>
2025-03-19 22:09:24 +0800

    Update configs.py

--- report_generation/configs.py
+++ report_generation/configs.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 
-neo4j_host = 'bolt://localhost:7687'
-neo4j_user = 'neo4j'
-neo4j_password = '12345678'
-neo4j_db = 'patents'
+neo4j_host = ''
+neo4j_user = ''
+neo4j_password = ''
+neo4j_db = ''
 
 max_history_len = 5
 
 # service_host = 'localhost'
-service_host = '0.0.0.0'
-service_port = 8081
+service_host = ''
+service_port = 
 
-# huggingface_token = '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'
-huggingface_token = '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'
+huggingface_token = ''
 
 openai_api_key = '' # FIXME: fill in the api key
 campus_api_key = '' # FIXME: fill in the api key
 
-tgi_host = 'http://localhost:9090'
+tgi_host = ''
